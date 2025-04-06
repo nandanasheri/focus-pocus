a62aa6f0d20669cd6998cc7836e6435dfc0b1026
Givikap <kaitosekiya@outlook.com>
2025-04-06 10:16:32 -0500

    Some frontend changes

--- frontend/components/navbar/navbar-mobile.tsx
+++ frontend/components/navbar/navbar-mobile.tsx
@@ -1,12 +1,11 @@
 "use client";
 
-import { NavbarUserLinks } from "@/components/navbar/navbar-user-links";
 import { buttonVariants } from "@/components/ui/button";
+import Link from "next/link";
 import {
   NavigationMenu,
   NavigationMenuContent,
   NavigationMenuItem,
-  NavigationMenuLink,
   NavigationMenuList,
   NavigationMenuTrigger,
 } from "@/components/ui/navigation-menu";
@@ -22,14 +21,10 @@ export const NavbarMobile = () => {
               <MenuIcon />
             </NavigationMenuTrigger>
             <NavigationMenuContent className="flex flex-col p-1">
-              <NavigationMenuLink
-                href="#1"
-                className={buttonVariants({ variant: "link" })}
-              >
-                Loserboard
-              </NavigationMenuLink>
               <div className="flex flex-col mb-0.5">
-                <NavbarUserLinks />
+                <Link href="/app" className={buttonVariants()}>
+                  Dashboard
+                </Link>
               </div>
             </NavigationMenuContent>
           </NavigationMenuItem>
