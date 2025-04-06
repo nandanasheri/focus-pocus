e8b9addccc30c64fc801476fb4309cca376942b3
Givikap <kaitosekiya@outlook.com>
2025-04-06 10:27:08 -0500

    Smallest change possible

--- frontend/app/(main)/page.tsx
+++ frontend/app/(main)/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
         <section className="space-y-5">
           <div className="container flex flex-col items-center gap-5 text-center">
             <h1 className="max-w-4xl font-heading font-semibold text-5xl text-accentblue sm:text-6xl md:text-6xl lg:text-8xl tracking-tighter">
-              Lock 0ut, Explore Your Traffic.
+              Lock0ut, Explore Your Traffic.
             </h1>
             <p className="max-w-2xl leading-normal text-muted-foreground text-semibold sm:text-xl sm:leading-8">
               A web based application that helps you to monitor your network traffic to warn you about your brain rot doom scrolling every day.
