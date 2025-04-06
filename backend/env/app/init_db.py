a77159d1a1e63c16bc1a521454d2da9b30525dcc
Nandana <nandanasheri@gmail.com>
2025-04-06 10:07:26 -0500

    alerts possible fix

--- backend/env/app/init_db.py
+++ backend/env/app/init_db.py
@@ -11,18 +11,18 @@ cursor.execute('''CREATE TABLE IF NOT EXISTS traffic (
                     destination_ip VARCHAR(50),
                     destination_name VARCHAR(100))''')
 
-# file_path = 'output.json'
+file_path = 'output.json'
 
-# # Open and read the JSON file
-# with open(file_path, 'r') as file:
-#     data = json.load(file)
+# Open and read the JSON file
+with open(file_path, 'r') as file:
+    data = json.load(file)
 
-# for row in data:
-#     conn.execute('INSERT INTO traffic (time, src_ip, destination_ip, destination_name) VALUES (?, ?, ?, ?) ', (row['time'], row['source_ip'], row['destination_ip'], ''))
+for row in data:
+    conn.execute('INSERT INTO traffic (time, src_ip, destination_ip, destination_name) VALUES (?, ?, ?, ?) ', (row['time'], row['source_ip'], row['destination_ip'], ''))
 
-# conn.commit() 
+conn.commit() 
 
-# rows = conn.execute('SELECT * FROM traffic').fetchall()
+rows = conn.execute('SELECT * FROM traffic').fetchall()
 
 # Close the connection
 conn.close()
