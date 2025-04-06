cd1703fd2a4530450bea29852640c91683730571
Givikap <kaitosekiya@outlook.com>
2025-04-06 00:52:56 -0500

    Fix merge conflict

--- frontend/app/(main)/(dashboard)/app/page.tsx
+++ frontend/app/(main)/(dashboard)/app/page.tsx
@@ -1,6 +1,4 @@
 import { DemoDashboard } from "@/components/demo-dashboard/demo-dashboard";
-<<<<<<< HEAD
-=======
 import {
   Card,
   CardHeader,
@@ -42,149 +40,125 @@ const recentActivity = [
   },
 
 ]
->>>>>>> page-dashboard
 
 const ApplicationPage = () => {
   return (
     <>
-<<<<<<< HEAD
-    <div className="flex-col md:flex">
-      <div className="flex items-end justify-between space-y-2 mb-6">
-        <h2 className="text-3xl leading-5 font-bold tracking-tight">
-          Dashboard
-        </h2>
-      </div>
-    </div>
-       
-=======
-    <div className="flex-col md:flex space-y-4" >
-      <div className="flex items-end justify-between space-y-2 mb-6">
-        <h2 className="text-6xl leading-5 font-bold tracking-tight text-accentblue">
-          Dashboard
-        </h2>
-      </div>
-      <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-8">
-      <Card className="col-span-4">
-        <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-          <CardTitle className="text-sm font-medium">
-            Recent Activity
-          </CardTitle>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              className="h-4 w-4 text-muted-foreground"
-            >
-            {/* <path d="M12 2v20M17 5H9.5a3.5 3.5 0 0 0 0 7h5a3.5 3.5 0 0 1 0 7H6" /> */}
-            </svg>
-            </CardHeader>
-            <CardContent>
-              <Table>
-                 <TableBody>
-                  {recentActivity.map((numId) => (
-                  <TableRow key={numId.numId}>
-                    <TableCell className="font-medium">{numId.numId}</TableCell>
-                    <TableCell>{numId.appName}</TableCell>
-                    <TableCell>{numId.hours}</TableCell>
-                  </TableRow>
-                  ))}
-                </TableBody>
-              </Table>
-            </CardContent>
-        </Card>
+      <div className="flex-col md:flex space-y-4" >
+        <div className="flex items-end justify-between space-y-2 mb-6">
+          <h2 className="text-6xl leading-5 font-bold tracking-tight text-accentblue">
+            Dashboard
+          </h2>
+        </div>
+        <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-8">
         <Card className="col-span-4">
-              <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-                <CardTitle className="text-sm font-medium">
-                  Total sites visited
-                </CardTitle>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  stroke="currentColor"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  className="h-4 w-4 text-muted-foreground"
-                >
-                  {/* <path d="M12 2v20M17 5H9.5a3.5 3.5 0 0 0 0 7h5a3.5 3.5 0 0 1 0 7H6" /> */}
-                </svg>
+          <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+            <CardTitle className="text-sm font-medium">
+              Recent Activity
+            </CardTitle>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                className="h-4 w-4 text-muted-foreground"
+              />
               </CardHeader>
               <CardContent>
-                <div className="text-2xl font-bold">example num</div>
-               {/* <p className="text-xs text-muted-foreground">
-                  
-                </p> */}
+                <Table>
+                  <TableBody>
+                    {recentActivity.map((numId) => (
+                    <TableRow key={numId.numId}>
+                      <TableCell className="font-medium">{numId.numId}</TableCell>
+                      <TableCell>{numId.appName}</TableCell>
+                      <TableCell>{numId.hours}</TableCell>
+                    </TableRow>
+                    ))}
+                  </TableBody>
+                </Table>
               </CardContent>
-            </Card>
-            </div>
-            <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-7">
-            <Card className="flex flex-col justify-between col-span-3 bg-accentpinkish">
-              <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-                <CardTitle className="text-3xl font-medium">
-                  Top site visited
-                </CardTitle>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  stroke="currentColor"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  className="h-4 w-4 text-muted-foreground"
-                >
-                  {/* <path d="M12 2v20M17 5H9.5a3.5 3.5 0 0 0 0 7h5a3.5 3.5 0 0 1 0 7H6" /> */}
-                </svg>
+          </Card>
+          <Card className="col-span-4">
+                <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+                  <CardTitle className="text-sm font-medium">
+                    Total sites visited
+                  </CardTitle>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    className="h-4 w-4 text-muted-foreground"
+                  />
+                </CardHeader>
+                <CardContent>
+                  <div className="text-2xl font-bold">example num</div>
+                </CardContent>
+              </Card>
+              </div>
+              <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-7">
+              <Card className="flex flex-col justify-between col-span-3 bg-accentpinkish">
+                <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+                  <CardTitle className="text-3xl font-medium">
+                    Top site visited
+                  </CardTitle>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    className="h-4 w-4 text-muted-foreground"
+                  />
+                </CardHeader>
+                <CardContent>
+                <p className="text-5xl font-bold">
+                example num
+                  </p>
+                </CardContent>
+              </Card>
+              <Card className="col-span-4">
+          <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+            <CardTitle className="text-sm font-medium">
+              Recent Activity
+            </CardTitle>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                className="h-4 w-4 text-muted-foreground"
+              />
               </CardHeader>
               <CardContent>
-                {/* <div className="text-2xl font-bold"></div> */}
-              <p className="text-5xl font-bold">
-              example num
-                </p>
+                <Table>
+                  <TableBody>
+                    {recentActivity.map((numId) => (
+                    <TableRow key={numId.numId}>
+                      <TableCell className="font-medium">{numId.numId}</TableCell>
+                      <TableCell>{numId.appName}</TableCell>
+                      <TableCell>{numId.hours}</TableCell>
+                    </TableRow>
+                    ))}
+                  </TableBody>
+                </Table>
               </CardContent>
-            </Card>
-            <Card className="col-span-4">
-        <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-          <CardTitle className="text-sm font-medium">
-            Recent Activity
-          </CardTitle>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              className="h-4 w-4 text-muted-foreground"
-            >
-            {/* <path d="M12 2v20M17 5H9.5a3.5 3.5 0 0 0 0 7h5a3.5 3.5 0 0 1 0 7H6" /> */}
-            </svg>
-            </CardHeader>
-            <CardContent>
-              <Table>
-                 <TableBody>
-                  {recentActivity.map((numId) => (
-                  <TableRow key={numId.numId}>
-                    <TableCell className="font-medium">{numId.numId}</TableCell>
-                    <TableCell>{numId.appName}</TableCell>
-                    <TableCell>{numId.hours}</TableCell>
-                  </TableRow>
-                  ))}
-                </TableBody>
-              </Table>
-            </CardContent>
-        </Card>
-
-            </div>
-            </div>
->>>>>>> page-dashboard
+          </Card>
+        </div>
+      </div>
     </>
   );
 };
+
 export default ApplicationPage;
